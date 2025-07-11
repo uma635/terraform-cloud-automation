0ea7900bbe33cfbae5ea6d4ccab948086953d64d
Umashankar git <umashankarmaurya567@gmail.com>
2025-07-11 19:04:52 +0530

    first-commite

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
 terraform {
     required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.0"
+        azure = {
+            source  = "hashicorp/azurerm"
+            version = "~> 3.0"
         }
     }
-    
-    required_version = ">= 0.12"
-}
-provider "aws" {
-    region = "us-west-2"
 }
 
-
-resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-    bucket = aws_s3_bucket.my_bucket.id
-
-    versioning_configuration {
-        status = "Enabled"
+provider "azurerm" {
+features {}
     }
+
+resource "azurerm_resource_group" "example" {
+    name     = "example-resources"
+    location = "West Europe"
 }
+
+
+
+
